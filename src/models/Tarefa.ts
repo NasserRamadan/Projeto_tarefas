b433bf2faecfd8d1d1e226a97e351b20402b33b1
Nasser Ramadan <nasserramadan22@gmail.com>
2024-05-24 10:33:12 -0300

    Realizando alterações e criando configurações

---
+++ src/models/Tarefa.ts
@@ -0,0 +1,26 @@
+import * as enums from '../utils/enums/tarefa'
+
+class Tarefa {
+  titulo: string
+  prioridade: enums.Prioridade
+  estado: enums.Status
+  descricao: string
+  id: number
+  status: enums.Status | undefined
+
+  constructor(
+    titulo: string,
+    prioridade: enums.Prioridade,
+    estado: enums.Status,
+    descricao: string,
+    id: number
+  ) {
+    this.titulo = titulo
+    this.prioridade = prioridade
+    this.estado = estado
+    this.descricao = descricao
+    this.id = id
+  }
+}
+
+export default Tarefa
